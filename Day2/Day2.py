1ba6ef8644ce1ed157b3eceed49753386fe2c5b9
Peter Kang <pxk7062@rit.edu>
2022-12-03 19:09:51 -0500

    Day 2 Part 2

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,4 +1,4 @@
-def pointCalculation( opponetPick, yourPick ):
+def pointCalculationPartOne( opponetPick, yourPick ):
     points = 0
     #inital points from pick
     yourPickPoints = {  'X': 1,'Y': 2,'Z': 3}
@@ -20,7 +20,55 @@ def partOne():
     # C Z 3 Scissor
     for matchUp in Lines:
         line = matchUp.strip().split(' ')
-        score = score+ pointCalculation(line[0],line[1])
+        score = score+ pointCalculationPartOne(line[0],line[1])
     print(score)
 
 partOne()
+
+def pointCalculationPartTwo( opponetPick, matchResult ):
+    # Match Result
+    # X Lose
+    # Y Draw
+    # Z Win
+    # Values
+    # A 1 Rock
+    # B 2 Paper
+    # C 3 Scissor
+    points = 0
+    #determine points from match up
+    yourPick = ''
+    if(matchResult == 'X'): #lose
+        points = 0
+        if opponetPick == 'A':
+            yourPick = 'C'
+        elif opponetPick == 'B':
+            yourPick = 'A'
+        else: #C
+            yourPick = 'B'
+    elif(matchResult == 'Y'): #draw
+        points = 3
+        yourPick = opponetPick
+    else: #Z or win
+        points = 6
+        if opponetPick == 'A':
+            yourPick = 'B'
+        elif opponetPick == 'B':
+            yourPick = 'C'
+        else: #C
+            yourPick = 'A'
+    #points from pick
+    yourPickPoints = {  'A': 1,'B': 2,'C': 3}
+    points = points + yourPickPoints[yourPick]
+
+    return points
+
+def partTwo():
+    strategy = open('input.txt', 'r')
+    Lines = strategy.readlines()
+    score = 0
+    for matchUp in Lines:
+        line = matchUp.strip().split(' ')
+        score = score+ pointCalculationPartTwo(line[0],line[1])
+    print(score)
+
+partTwo()

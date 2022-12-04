577afd473aa571b1f9d29b5bd00c65371149054e
Peter Kang <pxk7062@rit.edu>
2022-12-04 16:26:11 -0500

    updated day 4 part 1, done in a bounds checking way

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,23 +1,20 @@
+import re 
+
 def partOne():
     pairingFile = open('input.txt', 'r')
     pairings = pairingFile.readlines()
 
     fullContiansCount = 0
+
     for line in pairings:
         Ranges = line.strip().split(',')
-        
         firstRange = Ranges[0].split('-')
-        firstRange = range( int(firstRange[0]), int(firstRange[1])+1 )
-        firstRange = ("".join(map(str, firstRange))).strip()
-        
         secondRange = Ranges[1].split('-')
-        secondRange = range( int(secondRange[0]), int(secondRange[1])+1 )
-        secondRange = ("".join(map(str, secondRange))).strip()
 
-        #print(firstRange,',',secondRange)
-        if( firstRange.find(secondRange) != -1 or secondRange.find(firstRange) != -1 ):
+        if(  (int(firstRange[0]) <= int(secondRange[0]) and  int(firstRange[1]) >= int(secondRange[1]) ) or
+        (int(secondRange[0]) <= int(firstRange[0]) and  int(secondRange[1]) >= int(firstRange[1]) ) ):
             fullContiansCount = fullContiansCount+1
-        
+
     print(fullContiansCount)
         
         

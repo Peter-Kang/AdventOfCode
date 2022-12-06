1c73f11d5fbb5d25252a910b94ea9ee6cb5a3033
Peter Kang <pxk7062@rit.edu>
2022-12-06 13:57:51 -0500

    Day 6 part 2

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -23,4 +23,31 @@ def partOne():
             return i+1
     return -1
 
+def partTwo():
+    markerFile = open("input.txt","r")
+    marker = markerFile.readlines()
+    markerMessage = marker[0]
+    charSizeOfMarker = 14
+
+    currentMarker = ""
+    for i in range(len(markerMessage)):
+        if(len(currentMarker) == charSizeOfMarker):
+            currentMarker = currentMarker[1:charSizeOfMarker]
+        #add on the letter to the marker
+        currentMarker = currentMarker + markerMessage[i]
+        #print (currentMarker, i)
+        endMarker = False
+        if( len(currentMarker) == charSizeOfMarker ):
+            for j in currentMarker:
+                if(currentMarker.count(j) != 1):
+                    endMarker = False
+                    break
+                else:
+                    endMarker = True
+        if( endMarker):
+            print(i+1)
+            return i+1
+    return -1
+
 partOne()
+partTwo()

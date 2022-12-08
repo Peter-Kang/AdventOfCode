a97c5f394df1a83659890ee91d5b06c6350ce574
Peter Kang <pxk7062@rit.edu>
2022-12-08 17:32:18 -0500

    Day 8

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -55,5 +55,61 @@ def partOne():
     #print (visableFromEdgeInterior)
     print(visableCounter)
 
+def partTwo():
+    treetopFile = open('input.txt', 'r')
+    treetops = treetopFile.readlines()
 
-partOne()
+    row = len(treetops)
+    col = len(treetops[0].strip())
+
+    #create the 2-D array
+    values = []
+    for i in range (row):
+        colarray = []
+        for j in range(col):
+            colarray.append(0)
+        values.append(colarray)
+
+    highestValue = 0
+
+    for i in range (0, row):
+        for j in range (0, col):
+            currentValue = treetops[i][j]
+            #check up
+            upValue = 0
+            if( i != 0 ):
+                for up in range(i-1,-1,-1):
+                    upValue = upValue+1
+                    if treetops[up][j] >= currentValue:
+                        break
+            #check down
+            downValue = 0
+            if ( i != row ):
+                for down in range(i+1, row, 1):
+                    downValue = downValue+1
+                    if treetops[down][j] >= currentValue:
+                        break
+            #check left
+            leftValue = 0
+            if ( j != 0 ):
+                for left in range(j-1,-1,-1):
+                    leftValue =leftValue+1
+                    if treetops[i][left] >= currentValue:
+                        break
+            #check right
+            rightValue = 0
+            if( j!= col ):
+                for right in range(j+1, col, 1):
+                    rightValue = rightValue+1
+                    if treetops[i][right] >= currentValue:
+                        break
+            #print(upValue,downValue, leftValue, rightValue)
+            values[i][j] = upValue * downValue * leftValue * rightValue
+            if values[i][j] > highestValue:
+                highestValue = values[i][j] 
+    #print( values )
+    print(highestValue)
+
+
+#partOne()
+partTwo()

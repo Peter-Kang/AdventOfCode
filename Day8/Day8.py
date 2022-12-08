73fd12d31a6e8726873055a54e852a8e16d98263
Peter Kang <pxk7062@rit.edu>
2022-12-08 16:59:38 -0500

    Day 8 part 1

---
+++ Day8/Day8.py
@@ -0,0 +1,59 @@
+def partOne():
+    treetopFile = open('input.txt', 'r')
+    treetops = treetopFile.readlines()
+
+    row = len(treetops)
+    col = len(treetops[0].strip())
+    #print(row, col)
+    #print(treetops)
+    #create the 2-D array
+    visableFromEdgeInterior = []
+    for i in range (row):
+        colarray = []
+        for j in range(col):
+            colarray.append(0)
+        visableFromEdgeInterior.append(colarray)
+
+    #up and down
+    for i in range (1,col-1):
+        currentHighestValue = treetops[0][i]
+        #check line of sight
+        for j in range (1,row-1):
+            if currentHighestValue < treetops[j][i]:
+                #print("TopDown",j,i, currentValue,treetops[j][i] )
+                visableFromEdgeInterior[j][i] = 1
+                currentHighestValue = treetops[j][i]
+                #print(currentValue)
+        currentHighestValue = treetops[row-1][i]
+        for j in range (row-2, 0, -1):
+            if currentHighestValue < treetops[j][i]:
+                #print("BottomUp",j,i, currentValue,treetops[j][i] )
+                visableFromEdgeInterior[j][i] = 1
+                currentHighestValue = treetops[j][i]
+        
+    #left and right
+    for i in range(1,row-1):
+        currentHighestValue = treetops[i][0]
+        #check line of sight
+        for j in range (1, col-1):
+            if currentHighestValue < treetops[i][j]:
+                visableFromEdgeInterior[i][j] = 1
+                currentHighestValue = treetops[i][j]
+
+        currentHighestValue = treetops[i][col-1]
+        for j in range(col-2,0, -1):
+            if currentHighestValue < treetops[i][j]:
+                #print("right to left",i,j,currentHighestValue, treetops[i][j])
+                visableFromEdgeInterior[i][j] = 1
+                currentHighestValue = treetops[i][j]
+    
+    visableCounter = (2*row) + (2* (col-2))
+    #count up all the instances of 1
+    for i in range(row):
+        for j in range(col):
+            visableCounter = visableCounter+visableFromEdgeInterior[i][j]
+    #print (visableFromEdgeInterior)
+    print(visableCounter)
+
+
+partOne()
